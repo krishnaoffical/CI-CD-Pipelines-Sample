67ef43acd5abaf0bf9ff15ce807f4aa502a32f33
Bala <balakrishnanv2002@gmail.com>
2026-05-13 11:09:35 +0530

    Added few new test cases

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import unittest
 from app import add
+from app import sub
 
 class TestApp(unittest.TestCase):
 
@@ -11,6 +12,12 @@ class TestApp(unittest.TestCase):
     
     def test_add2(self):
         self.assertAlmostEqual(add(-1,9),8)
+    
+    def test_sub1(self):
+        self.assertAlmostEqual(sub(-1,9),-10)
+
+    def testsub2(self):
+        self.assertAlmostEqual(sub(2,9),-7)
 
 if __name__ == "__main__":
     unittest.main()
